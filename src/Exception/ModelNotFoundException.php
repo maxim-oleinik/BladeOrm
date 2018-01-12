f783612949641592bcdc857f037da747d97583e0
Kasimov Arthur <kasimovarthur@gmail.com>
2018-01-12 11:37:11 +0300

    change ModelNotFoundException and related classes

--- src/Exception/ModelNotFoundException.php
+++ src/Exception/ModelNotFoundException.php
@@ -1,7 +1,35 @@
 <?php namespace BladeOrm\Exception;
 
 
+use BladeOrm\Table;
+
 class ModelNotFoundException extends \Exception
 {
+    /**
+     * @var Table хранит класс таблицы, связанный с исключением
+     */
+    protected $table;
+
+    /**
+     * @return mixed
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
+     * ModelNotFoundException constructor.
+     * @param Table $table - обязателен для того, чтобы можно было олбработать информацию, связанную с классом таблицы.
+     *      Например вывести ошибку пользователю
+     * @param string $message
+     * @param int $code
+     * @param null $previous
+     */
+    public function __construct(Table $table, $message = "", $code = 0, $previous = null)
+    {
+        $this->table=$table;
+        parent::__construct($message, $code, $previous);
+    }
 
 }
